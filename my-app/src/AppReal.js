b7788f22803050ee8f4ff556997fa2804d415de3
Gregory Tang <gtang159@gmail.com>
2019-11-07 17:50:03 -0800

    Fixed sizing for mobile responsiveness

--- my-app/src/AppReal.js
+++ my-app/src/AppReal.js
@@ -1,9 +1,11 @@
 import React from "react";
 import App from "./App";
+import "./style.css";
 
 const AppReal = () => {
   return (
-    <div>
+    <div className="app-wrap">
+      <div className="app-title">Welcome to React Chatrooms!</div>
       <App />
     </div>
   );
